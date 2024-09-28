352c2987820c5bf51604cece46bb06dd94b261e1
Uddict <uditkp024@gmail.com>
2024-09-28 20:10:28 +0530

    initialization

---
+++ src/components/counter.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 0,
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <span className={this.getClassCounter()}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+      </React.Fragment>
+    );
+  }
+
+  getClassCounter() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+}
+
+export default Counter;
